50474ad688d2b58f2392189d93f49525010ad4be
Erik Kennedy <erik@niload.com>
2019-09-04 21:43:57 +0000

    orly

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -2,21 +2,21 @@
 # Script to deploy a very simple web application.
 # The web app has a customizable image and some text.
 
-cat << EOM > /var/www/html/index.html
-<html>
-  <head><title>Meowy!</title></head>
-  <body>
-  <div style="width:800px;margin: 0 auto">
+# cat << EOM > /var/www/html/index.html
+# <html>
+#   <head><title>Meowy!</title></head>
+#   <body>
+#   <div style="width:800px;margin: 0 auto">
 
-  <!-- BEGIN -->
-  <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
-  <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own, or don't. 
-  <!-- END -->
+#   <!-- BEGIN -->
+#   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
+#   <center><h2>Meow World!</h2></center>
+#   Welcome to ${PREFIX}'s app. Replace this text with your own, or don't. 
+#   <!-- END -->
   
-  </div>
-  </body>
-</html>
-EOM
+#   </div>
+#   </body>
+# </html>
+# EOM
 
 echo "Script complete."
